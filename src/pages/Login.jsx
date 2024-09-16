9962bfca19fbebf8c3420f053f12a1ad28fa7a1a
SanguchoMela <melany01-03@hotmail.com>
2024-09-16 09:43:47 -0500

    feat: login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+    MDBContainer,
+    MDBCol,
+    MDBRow,
+    MDBBtn,
+    MDBInput
+}
+    from 'mdb-react-ui-kit';
+
+function App() {
+    return (
+        <MDBContainer fluid className="p-3 my-5">
+
+            <MDBRow>
+
+                <MDBCol col='10' md='6'>
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid" alt="Phone image" />
+                </MDBCol>
+
+                <MDBCol col='4' md='6'>
+
+                    <MDBInput wrapperClass='mb-4' label='Correo electrónico' id='formControlLg' type='email' size="lg" />
+                    <MDBInput wrapperClass='mb-4' label='Contraseña' id='formControlLg' type='password' size="lg" />
+
+                    <MDBBtn className="mb-4 w-100" size="lg">Iniciar Sesión</MDBBtn>
+
+                </MDBCol>
+        
+            </MDBRow>
+
+        </MDBContainer>
+    );
+}
+
+export default App;
