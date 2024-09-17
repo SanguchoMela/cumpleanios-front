655d1326e8aacd689d7808f0b287869b4330a12b
SanguchoMela <melany01-03@hotmail.com>
2024-09-17 14:53:43 -0500

    feat: react dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import App from './App.jsx'
-import './index.css'
+import App from "./App.jsx";
+import "./index.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
 )
