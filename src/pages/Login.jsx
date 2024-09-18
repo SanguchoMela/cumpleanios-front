064a29d4c4ac9629ebf1285e707d77d10159de81
SanguchoMela <melany01-03@hotmail.com>
2024-09-18 12:14:10 -0500

    feat: validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -33,6 +33,10 @@ function Login() {
         const username = formData.get("username");
         const password = formData.get("password");
 
+        if (username === "" || password === "" ){
+            return
+        }
+
         try {
             await login(username, password);
             navigate("/dashboard");
@@ -53,7 +57,7 @@ function Login() {
                 <MDBCol col='4' md='6'>
 
                     <form onSubmit={handleSubmit}>
-                        <MDBInput wrapperClass='mb-4' label='Usuario' id='formControlLg' type='email' name='username' size="lg" value={form.username} onChange={handleChange} />
+                        <MDBInput wrapperClass='mb-4' label='Usuario' id='formControlLg' type='text' name='username' size="lg" value={form.username} onChange={handleChange} />
                         <MDBInput wrapperClass='mb-4' label='Contraseña' id='formControlLg' type='password' name='password' size="lg" value={form.password} onChange={handleChange} />
 
                         <MDBBtn type='submit' className="mb-4 bg-blue-500" size="lg">Iniciar Sesión</MDBBtn>
