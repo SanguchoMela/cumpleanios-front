848ae6f0ff94d9989b99678a4934c68348159286
SanguchoMela <melany01-03@hotmail.com>
2024-09-18 12:17:46 -0500

    feat: report

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -3,7 +3,23 @@ import Table from '../components/Table';
 
 const Report = () => {
   return (
-    <Table/>
+    <>
+      <div className="mt-1">
+        <label
+          className="text-sm font-semibold"
+          htmlFor="fechaNacimiento"
+        >
+          Fecha de nacimiento
+        </label>
+        <input
+          className="py-2 px-2 w-full border border-turquesa-fuerte rounded-lg focus:outline-none focus:ring-1 focus:ring-turquesa-100"
+          id="fechaNacimiento"
+          type="date"
+          name="fechaNacimiento"
+        />
+      </div>
+      <Table />
+    </>
   );
 }
 
