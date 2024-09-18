e55cfae008be7207f11a75e8f0d022f22fe14b1d
SanguchoMela <melany01-03@hotmail.com>
2024-09-18 15:27:39 -0500

    feat: calendar

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Calendario from '../components/Calendario';
+import axios from 'axios';
+import moment from 'moment';
 
 export default function Calendar() {
+  const [events, setEvents] = useState([]);
+
+  const showBirth = async () => {
+    try {
+      const jwt = localStorage.getItem("jwt");
+      const url = `${import.meta.env.VITE_BACKEND_URL}/users`;
+      const options = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${jwt}`,
+        },
+      };
+      const response = await axios.get(url, options);
+      const empleados = response.data;
+      console.log(empleados)
+
+      const eventsFormat = empleados
+        .map((empleado, index) => {
+          if (empleado.birthDate) {
+            const birthDate = moment(empleado.birthDate);
+            const currentYear = moment().year();
+
+            const adjustedBirthDate = birthDate.year(currentYear).startOf('day');
+
+            return {
+              id: index,
+              title: `${empleado.name} ${empleado.lastName}`,
+              start: adjustedBirthDate.toDate(),
+              end: adjustedBirthDate.toDate(),
+            };
+          } else {
+            console.log("Error de formato");
+            return null;
+          }
+        })
+      console.log(eventsFormat)
+      setEvents(eventsFormat)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    showBirth()
+  }, [])
+
   return (
-    <Calendario/>
+    <Calendario events={events} />
   );
 }
