1867bcd0adb2d182adfc524815c10489f5a4dbfc
SanguchoMela <melany01-03@hotmail.com>
2024-09-18 12:56:10 -0500

    feat: register

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,19 @@
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { MDBBtn } from 'mdb-react-ui-kit';
 
 export default function Form() {
     const navigate = useNavigate()
     const [form, setForm] = useState({
         name: "",
-        lastname: "",
+        lastName: "",
+        birthDate: "",
         email: "",
-        birthDay: "",
     })
 
     const handleChange = (e) => {
+        e.preventDefault()
         setForm({
             ...form,
             [e.target.name]: e.target.value,
@@ -21,13 +23,19 @@ export default function Form() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const url = `${import.meta.env.VITE_BACKEND_URL}/registro`;
-            const response = await axios.post(url, form);
-            setTimeout(() => {
-                navigate("/dashboard/empleados");
-            }, 3000);
+            const jwt = localStorage.getItem("jwt")
+            const url = `${import.meta.env.VITE_BACKEND_URL}/users`;
+            const options = {
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${jwt}`,
+                },
+              };
+            const response = await axios.post(url, form, options);
+            window.alert("Registro exitoso")
         } catch (error) {
             console.log(error)
+            window.alert("Error al registrar")
         }
     }
 
@@ -38,51 +46,49 @@ export default function Form() {
                 <div className="flex justify-around gap-x-10 mx-6 flex-wrap md:flex-nowrap">
                     <div className="w-full">
                         <div>
-                            <label className="text-sm font-semibold" htmlFor="nombre">
+                            <label className="text-sm font-semibold" htmlFor="name">
                                 Nombre
                             </label>
                             <input
                                 className="py-2 pl-2 w-full border border-turquesa-fuerte rounded-lg focus:outline-none focus:ring-1 focus:ring-turquesa-100"
-                                id="nombre"
+                                id="name"
                                 type="text"
                                 placeholder="Ingresa el nombre"
-                                name="nombre"
+                                name="name"
                                 value={form.name || ""}
                                 onChange={handleChange}
                             />
                         </div>
                         <div className="mt-1">
-                            <label className="text-sm font-semibold" htmlFor="apellido">
+                            <label className="text-sm font-semibold" htmlFor="lastName">
                                 Apellido
                             </label>
                             <input
                                 className="py-2 pl-2 w-full border border-turquesa-fuerte rounded-lg focus:outline-none focus:ring-1 focus:ring-turquesa-100"
-                                id="apellido"
+                                id="lastName"
                                 type="text"
                                 placeholder="Ingresa el apellido"
-                                name="apellido"
-                                value={form.lastname || ""}
+                                name="lastName"
+                                value={form.lastName || ""}
                                 onChange={handleChange}
                             />
                         </div>
                         <div className="mt-1">
                             <label
                                 className="text-sm font-semibold"
-                                htmlFor="fechaNacimiento"
+                                htmlFor="birthDate"
                             >
                                 Fecha de nacimiento
                             </label>
                             <input
                                 className="py-2 px-2 w-full border border-turquesa-fuerte rounded-lg focus:outline-none focus:ring-1 focus:ring-turquesa-100"
-                                id="fechaNacimiento"
+                                id="birthDate"
                                 type="date"
-                                name="fechaNacimiento"
-                                value={form.birthDay || ""}
+                                name="birthDate"
+                                value={form.birthDate || ""}
                                 onChange={handleChange}
                             />
                         </div>
-                    </div>
-                    <div className="w-full">
                         <div>
                             <label className="text-sm font-semibold" htmlFor="email">
                                 Correo electrónico
@@ -100,17 +106,8 @@ export default function Form() {
                     </div>
                 </div>
                 <div className="m-6 flex justify-end">
-                    <input
-                        className="py-2 px-3 text-center text-sm font-semibold bg-blue-500 text-white rounded-xl hover:bg-blue-300 cursor-pointer"
-                        type="submit"
-                        value="Registrar"
-                    />
-                    <div
-                        className="ml-3 px-3 py-2 text-sm md:px-4 text-white font-semibold bg-orange-500 hover:bg-orange-300 rounded-xl cursor-pointer"
-                        onClick={() => navigate("/dashboard/empleados")}
-                    >
-                        Cancelar
-                    </div>
+                    <MDBBtn type="submit" className="mr-4 bg-blue-500" size="sm">Registrar</MDBBtn>
+                    <MDBBtn className="bg-orange-500" size="sm">Cancelar</MDBBtn>
                 </div>
             </form>
         </>
