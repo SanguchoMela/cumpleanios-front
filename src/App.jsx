03d3774029056199d168ff341c66e0c87318fc83
SanguchoMela <melany01-03@hotmail.com>
2024-09-18 10:10:06 -0500

    feat: router

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,28 @@ import Dashboard from './layout/Dashboard';
 import Calendar from './pages/Calendar';
 import Employees from './pages/Employees';
 import Report from './pages/Report';
-import Auth from './layout/Auth';
+import { AuthProvider } from './context/AuthProvider';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route index element={<Login />} />
-        <Route
-          path="/dashboard/*"
-          element={
-            <Routes>
-              <Route element={<Dashboard />}>
-                <Route index element={<Calendar />} />
-                <Route path='empleados' element={<Employees />} />
-                <Route path='reporte' element={<Report />} />
-              </Route>
-            </Routes>
-          }
-        />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route index element={<Login />} />
+          <Route
+            path="/dashboard/*"
+            element={
+              <Routes>
+                <Route element={<Dashboard />}>
+                  <Route index element={<Calendar />} />
+                  <Route path='empleados' element={<Employees />} />
+                  <Route path='reporte' element={<Report />} />
+                </Route>
+              </Routes>
+            }
+          />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
