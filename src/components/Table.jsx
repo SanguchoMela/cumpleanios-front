50a20c7f9335e9f6cc56d1845ffa1f2ec8ad0e43
SanguchoMela <melany01-03@hotmail.com>
2024-09-18 14:39:29 -0500

    feat: employees table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
 import { MdDelete } from "react-icons/md";
+import axios from 'axios';
 
 export default function Table({ urlActual }) {
+  const [employees, setEmployees] = useState([])
+
+  const listarEmpleados = async () => {
+    try {
+      const jwt = localStorage.getItem("jwt");
+      const url = `${import.meta.env.VITE_BACKEND_URL}/users`;
+      const options = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${jwt}`,
+        },
+      };
+      const response = await axios.get(url, options);
+      const empleados = response.data;
+      console.log(empleados)
+      setEmployees(empleados);
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    listarEmpleados()
+  }, [])
+
   return (
     <>
       <MDBTable>
@@ -18,15 +44,17 @@ export default function Table({ urlActual }) {
           </tr>
         </MDBTableHead>
         <MDBTableBody>
-          <tr>
-            <th scope='row'>1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            {urlActual === "/dashboard/empleados" && (
-              <td><MdDelete /></td>
-            )}
-          </tr>
+          {employees && employees.map((employee, index) => (
+            <tr key={employee.id}>
+              <th scope='row'>{employee.id}</th>
+              <td>{employee.name}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.dateBirth}</td>
+              {urlActual === "/dashboard/empleados" && (
+                <td><MdDelete /></td>
+              )}
+            </tr>
+          ))}
         </MDBTableBody>
       </MDBTable></>
   );
