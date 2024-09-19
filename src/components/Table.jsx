7e2761dae2b9a2e584a94a05a7f57518fe016f09
SanguchoMela <melany01-03@hotmail.com>
2024-09-19 12:59:08 -0500

    fix: pdf

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,9 +1,6 @@
 import axios from 'axios';
 import { MDBTable, MDBTableHead, MDBTableBody, MDBBtn } from 'mdb-react-ui-kit';
 import { MdDelete } from "react-icons/md";
-import jsPDF from 'jspdf';
-import 'jspdf-autotable';
-import moment from 'moment';
 
 export default function Table({ urlActual, empleados = [] }) {
   const arrayLength = empleados.length
@@ -29,44 +26,8 @@ export default function Table({ urlActual, empleados = [] }) {
     }
   }
 
-  const generatePDF = () => {
-    const doc = new jsPDF();
-    const currentMonth = moment().format('MMMM').toLowerCase()
-
-    doc.text("Lista de Empleados", 14, 22);
-
-    const columns = [
-      { header: "N°", dataKey: "numero" },
-      { header: "Nombre", dataKey: "name" },
-      { header: "Apellido", dataKey: "last_name" },
-      { header: "Teléfono", dataKey: "phone" },
-      { header: "Fecha de Nacimiento", dataKey: "dateBirth" },
-    ];
-
-    const rows = empleados.map((employee, index) => ({
-      numero: index + 1,
-      name: employee.name,
-      last_name: employee.last_name,
-      phone: employee.phone,
-      dateBirth: employee.dateBirth,
-    }));
-
-    doc.autoTable({
-      columns: columns,
-      body: rows,
-      startY: 30,
-    });
-
-    doc.save(`${currentMonth}.pdf`);
-  };
-
   return (
     <>
-      <div className='mb-4 flex justify-end'>
-        {urlActual === "/dashboard/reporte" && arrayLength > 0 && (
-          <MDBBtn className="bg-orange-500 w-36 hover:bg-orange-600" onClick={generatePDF} size="md">Descargar PDF</MDBBtn>
-        )}
-      </div>
       <MDBTable>
         <MDBTableHead>
           <tr>
--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Table from '../components/Table';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import moment from 'moment';
+import { MDBBtn } from 'mdb-react-ui-kit';
 
 const Report = () => {
   const location = useLocation()
@@ -36,6 +40,38 @@ const Report = () => {
       console.log(error)
     }
   }
+  
+  const generatePDF = () => {
+    const doc = new jsPDF();
+    const currentMonthBirthday = moment(monthEmployees[0].dateBirth).format('MMMM').toLowerCase()
+    console.log(currentMonthBirthday)
+
+    doc.text(`Empleados que cumplen años en ${currentMonthBirthday}`, 14, 22);
+
+    const columns = [
+      { header: "N°", dataKey: "id" },
+      { header: "Nombre", dataKey: "name" },
+      { header: "Apellido", dataKey: "last_name" },
+      { header: "Teléfono", dataKey: "phone" },
+      { header: "Fecha de Nacimiento", dataKey: "dateBirth" },
+    ];
+
+    const rows = monthEmployees.map((employee, index) => ({
+      id: index + 1,
+      name: employee.name,
+      last_name: employee.last_name,
+      phone: employee.phone,
+      dateBirth: employee.dateBirth,
+    }));
+
+    doc.autoTable({
+      columns: columns,
+      body: rows,
+      startY: 30,
+    });
+
+    doc.save(`${currentMonthBirthday}.pdf`);
+  };
 
   useEffect(() => {
     listarEmpleadosMes(selectedMonth)
@@ -43,6 +79,11 @@ const Report = () => {
 
   return (
     <>
+      <div className='mb-4 flex justify-end'>
+        {monthEmployees.length > 0 && (
+          <MDBBtn className="bg-orange-500 w-36 hover:bg-orange-600" onClick={generatePDF} size="md">Descargar PDF</MDBBtn>
+        )}
+      </div>
       <div className="mt-1 w-full flex justify-center mb-5">
         <label
           className="text-sm font-semibold mr-5"
